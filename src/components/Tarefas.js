f5e4a2b7070cbac219562010ee6c1b9e8dc82127
Henrique Almeida <marceloh500@gmail.com>
2023-09-11 22:52:30 -0300

    Inseri a função de adicionar tarefa

--- src/components/Tarefas.js
+++ src/components/Tarefas.js
@@ -2,16 +2,24 @@ import styles from './Tarefas.module.css'
 import {TiDeleteOutline} from 'react-icons/ti'
 import {TiEdit} from 'react-icons/ti'
 
-function Tarefas(){
+function Tarefas({lista}){
     return(
+        <div>
+            
+        
+            
+      
         <div className={styles.containerTarefa}>
-            <p>Nome da Tarefa</p>
-            <div className={styles.buttonsContainer}>
-            <button className={styles.buttonTarefas}><TiDeleteOutline/></button>
-            <button className={styles.buttonTarefas}><TiEdit/></button>
-            </div>
-        </div>
-
-    )
-}
+            {lista.map((item, index)=>(
+                <item className={styles.itensContainer}>{item}
+               <div className={styles.buttonsContainer}>
+                <button className={styles.buttonTarefas}><TiDeleteOutline/></button>
+                <button className={styles.buttonTarefas}><TiEdit/></button>
+                </div>
+               
+                </item>
+            ))}
+     </div>
+       </div>
+)}
 export default Tarefas;
--- src/components/pages/ToDoList.js
+++ src/components/pages/ToDoList.js
@@ -1,15 +1,33 @@
 import styles from './ToDoList.module.css'
 import Tarefas from './../Tarefas'
+import { useState } from 'react'
 
 function ToDoList(){
+
+    const [lista, setLista] = useState([])
+    const [novaTarefa, setNovaTarefa] = useState ('')  
+    const [vazio, setvazio] = useState('')
+
+    function adicionarTarefa(){
+        setLista([...lista, novaTarefa])
+        setNovaTarefa('')
+        setvazio('ok')
+    }  
+
+
     return(
         <div className={styles.container}>
             <h1>Lista de Tarefas</h1>
             <div className={styles.inputButton}>
-            <input type="text" placeholder="Adicionar Tarefa"/>
-            <button className={styles.buttonAdicionar}>Adicionar</button>
+            <input type="text" placeholder="Adicionar Tarefa" value={novaTarefa} onChange={(e)=>setNovaTarefa(e.target.value)}/>
+            <button className={styles.buttonAdicionar} onClick={adicionarTarefa}>Adicionar</button>
             </div>
-            <Tarefas/>
+            { vazio ?
+            <Tarefas lista={lista}/>
+        : null}
+            
+        
+        
 
         </div>
 
